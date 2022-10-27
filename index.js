e6795f01539b2aa0d7945cdfd868e31cd7800a98
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-10-27 15:57:33 +0600

    individual code got

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
 app.get("/courses", (req, res) => {
     res.send(courses)
 })
+app.get("/courses/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse)
+})
 app.listen(port, ()=> {
     console.log("App running on port", port)
 })
