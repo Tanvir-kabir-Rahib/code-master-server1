5654dc4f69409f67e0eada176c86874772bd2ce8
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-10-27 15:56:34 +0600

    courses got in ui

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express()
 const cors = require("cors")
 const port = process.env.PORT || 4000
 app.use(cors())
+const courses = require("./Data/courses.json")
 
 app.get("/", (req, res) => {
-    res.send("hello World")
+    res.send("Code master server running")
+})
+app.get("/courses", (req, res) => {
+    res.send(courses)
 })
 app.listen(port, ()=> {
     console.log("App running on port", port)
