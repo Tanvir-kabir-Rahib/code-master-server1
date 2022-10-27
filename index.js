cc5b0123363b203b37c2adb8d854fe7001bddda8
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-10-27 15:53:24 +0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const port = process.env.PORT || 4000
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("hello World")
